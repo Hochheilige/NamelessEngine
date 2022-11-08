1c98df354598254e8f0a5ea01205e39833b31bce
Yuri Tcherezov <tcherezovyuri@mail.ru>
2022-11-08 23:42:42 +0300

    - Added delegates that Zagarskih provided at some point

--- GameFramework/EditorContext.cpp
+++ GameFramework/EditorContext.cpp
@@ -2,6 +2,17 @@
 
 #include "Component.h"
 
+auto EditorContext::BindToOnSelectedComponentChanged(SelectedComponentChangedDelegate Delegate) -> DelegateHandle
+{
+	return OnSelectedComponentChanged.Add(std::move(Delegate));
+}
+
+auto EditorContext::UnbindFromSelectedComponentChanged(DelegateHandle& handle) -> void
+{
+	OnSelectedComponentChanged.Remove(handle);
+}
+
+
 auto EditorContext::SetSelectedActor(Actor* InActor) -> void
 {
 	SelectedActor = InActor;
