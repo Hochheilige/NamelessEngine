be3b30400b18376b87f696c63df3177dc12dbdc9
DarkBat52 <thezoolion@gmail.com>
2023-01-03 12:55:29 +0300

    - added root node file

---
+++ Scripts/BehaviorTree/RootTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.BehaviorTree
+{
+    public class RootTask : Task
+    {
+        List<Task> tasks = new List<Task>();
+        public override bool run()
+        {
+            return true;
+        }
+    }
+}
