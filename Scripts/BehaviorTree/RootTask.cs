6bbdb8d55174a25a5aa4b3b252306cc6af1587a7
DarkBat52 <thezoolion@gmail.com>
2023-01-04 22:52:44 +0300

    - Added duplicate and some BT update

--- Scripts/BehaviorTree/RootTask.cs
+++ Scripts/BehaviorTree/RootTask.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Scripts.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,19 @@ namespace Scripts.BehaviorTree
     public class RootTask : Task
     {
         List<Task> tasks = new List<Task>();
-        public override bool run()
+
+        Task child;
+
+        public TaskStateEnum Execute(BehaviorTreeComponent btComponent, object nodeMemory)
         {
-            return true;
+            foreach (var task in tasks)
+            {
+                task.Execute(btComponent, nodeMemory);
+            }
+
+            return TaskStateEnum.Succeded ;
         }
+
+
     }
 }
---
+++ Scripts/Components/BehaviorTreeComponent.cs
@@ -0,0 +1,28 @@
+﻿using Scripts.BehaviorTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scripts.Components
+{
+    public class BehaviorTreeComponent
+    {
+        public float deltaTime;
+
+        private List<Task> tasks = new List<Task>();
+
+        private RootTask root;
+
+        public void AddTask(Task task)
+        {
+            tasks.Add(task);
+        }
+
+        public TaskStateEnum Execute()
+        {
+            return root.Execute(this, null);
+        }
+
+    }
+}
